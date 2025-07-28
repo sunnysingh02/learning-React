eaa9a7e6b6a4568dfbd6e689ac731254200c2f7e
sunny singh <sunny2005sin@gmail.com>
2025-07-28 12:52:04 +0530

    creating dynamic profile list using .map()

---
+++ OneDrive/Desktop/3.0/week_9/react/src/App.jsx
@@ -0,0 +1,59 @@
+
+
+function App() {
+  const users = [
+  {
+    userName: "sunny05",
+    name: "Sunny Singh",
+    avatarImage: "https://randomuser.me/api/portraits/men/32.jpg",
+    bio: "MERN Stack Developer | 100xDev Learner",
+  },
+  {
+    userName: "priya07",
+    name: "Priya Sharma",
+    avatarImage: "https://randomuser.me/api/portraits/women/65.jpg",
+    bio: "UI/UX Designer | Loves Figma & React",
+  },
+  {
+    userName: "rahul09",
+    name: "Rahul Mehta",
+    avatarImage: "https://randomuser.me/api/portraits/men/76.jpg",
+    bio: "Backend Wizard | Node.js & Databases",
+  },
+];
+
+
+  return (
+    <div>
+      {users.map(user=>(
+        <ProfileCard
+        userName={user.userName}
+        avatarImage={user.avatarImage}
+        name={user.name}
+        bio={user.bio}
+        />
+      ))}
+       
+      
+    </div>
+  );
+}
+
+const ProfileCard = ({ userName, avatarImage, name, bio }) => {
+  return (
+    <div>
+      <h1>userName:- {userName}</h1>
+      <form style={{backgroundColor: "", border: '2px dotted #4CAF50', padding: '20px', margin: '10px', borderRadius: '8px'}}>
+        <img src={avatarImage} style={{ width: 50, height: 50, }} /><br />
+        <b>{name}</b>
+        <p>{bio}</p>
+
+      </form>
+
+    </div>
+  )
+
+
+}
+
+export default App;
